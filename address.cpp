33207d2b9cf536e2f749873380be0e108d7ffb99
Marianne Adams <meadams2@bsu.edu>
2025-02-21 15:07:09 +0000

    Fixed Occam's Razor

--- address.cpp
+++ address.cpp
@@ -13,42 +13,11 @@ Address::Address(){
 	Address::zip = "";
 } //end constructor
 
-void Address::init(std::string addressString){
-	std::stringstream converter;
-	
-	std::string street;
-	std::string city;
-	std::string state;
-	std::string zip;
-
-	Address::street;
-	Address::city;
-	Address::state;
-	Address::zip;
-
-	converter.clear();
-	converter.str(addressString);
-
-	getline(converter, street, ',');
-	getline(converter, city, ',');
-	getline(converter, state, ',');
-	getline(converter, zip);
-
-	converter.clear();
-	converter.str(street);
-	converter >> Address::street;
-
-	converter.clear();
-	converter.str(city);
-	converter >> Address::city;
-
-	converter.clear();
-	converter.str(state);
-	converter >> Address::state;
-	
-	converter.clear();
-	converter.str(zip);
-	converter >> Address::zip;
+void Address::init(std::string street, std::string city, std::string state, std::string zip){
+	Address::street = street;
+	Address::city = city;
+	Address::state = state;
+	Address::zip = zip;
 } //end init
 
 void Address::printAddress(){
--- address.h
+++ address.h
@@ -14,7 +14,7 @@ class Address {
 		std::string zip;
 	public:
 		Address();
-		void init(std::string addressString);
+		void init(std::string street, std::string city, std::string state, std::string zip);
 		void printAddress();
 }; // end class def
 
